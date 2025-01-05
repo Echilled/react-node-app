cbbac8c5bc2ca39adac39815948a032a6278d238
Echilled <echew140899@gmail.com>
2025-01-05 18:39:43 +0800

    front-end changes

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
         {!data ? "Loading..." : data}
@@ -26,7 +26,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
       <LoginSignup/>
     </div>
   );
